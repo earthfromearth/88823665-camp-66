30d4b5accb10edb5ea469c9d8036e9c9917ca0f2
earthfromearth <suphanutpangot@gmail.com>
2025-01-14 18:59:40 +0700

    laravel 101

---
+++ mylaravel/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+
+Route::get('/mycontroller/{id?}' ,
+    [MyController::class, 'myfunction']);
+
+Route::post('/mycontroller/{id?}' ,
+    [MyController::class, 'myfunction']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/hello/{id?}',
+    function ($val="") {
+    return "<h1>Hello World $val</h1>";
+});
